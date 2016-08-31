b4214babcec807860e346258123e5fcfc93e4215
Julián Cortés <pity7736@gmail.com>
2016-08-31 15:42:30 -0500

    mandadas views tests

--- mandamejor/mandadas/tests/test_mandada_view.py
+++ mandamejor/mandadas/tests/test_mandada_view.py
@@ -15,6 +15,7 @@ class MandadaViewTests(TestCase):
             Mandada(user=user2, when='2016-08-25'),
             Mandada(user=user2, when='2016-08-26'),
             Mandada(user=user2, when='2016-08-28'),
+            Mandada(user=user1, when='2016-08-28'),
         ))
 
     def setUp(self):
@@ -23,22 +24,29 @@ class MandadaViewTests(TestCase):
     def test_get_mandadas(self):
         response = self.client.get('/mandadas/')
 
-        self.assertEqual(len(response.data), 5)
+        self.assertEqual(len(response.data), 6)
         self.assertEqual(response.status_code, 200)
 
-    def test_get_mandadas_a_content(self):
+    def test_get_mandadas_no_content(self):
         Mandada.objects.all().delete()
         response = self.client.get('/mandadas/')
 
         self.assertEqual(len(response.data), 0)
         self.assertEqual(response.status_code, 204)
 
+    def test_search_mandadas_by_when_date(self):
+        response = self.client.get('/mandadas/search/', {'when': '2016-08-28'})
+
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0]['when'], '2016-08-28')
+
     def test_get_mandadas_by_user(self):
         response = self.client.get('/mandadas/user/1/')
 
-        self.assertEqual(len(response.data), 2)
+        self.assertEqual(len(response.data), 3)
         self.assertEqual(response.data[0]['user'], 1)
         self.assertEqual(response.data[1]['user'], 1)
+        self.assertEqual(response.data[2]['user'], 1)
         self.assertEqual(response.status_code, 200)
 
     def test_get_mandadas_by_nonexistent_user(self):
